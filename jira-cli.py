b4940a4a9887fdc7f5bfa2469e1f186eb7a4e03d
Christopher Su <christophersu9@gmail.com>
2014-07-12 16:40:44 -0700

    progress

---
+++ jira-cli.py
@@ -0,0 +1,51 @@
+from jira.client import JIRA
+import click
+from secret import *
+
+jira = JIRA(options={'server': JIRA_SERVER}, basic_auth=(JIRA_USERNAME, JIRA_PASSWORD))
+
+@click.group()
+def cli1():
+    pass
+
+@cli1.command()
+# @click.option('--project', '-p', default='PER', help='project key')
+@click.option('--type', '-t', default='Task', help='issue type name')
+@click.option('--epic', '-e', help='an epic to link to (issue id)')
+@click.argument('project')
+@click.argument('summary')
+def create(project, type, epic, summary):
+    """Create an issue"""
+    issue_dict = {
+        'project': {'key': project},
+        'issuetype': {'name': type},
+        'summary': summary,
+    }
+    if epic:
+        issue_dict['customfield_10001'] = epic
+    new_issue = jira.create_issue(fields=issue_dict)
+
+@cli1.command()
+# @click.option('--project', '-p', default='PER', help='project key')
+@click.option('--type', '-t', default='Task', help='issue type name')
+@click.option('--epic', '-e', help='an epic to link to (issue id)')
+@click.argument('project')
+@click.argument('filename')
+def batch(project, epic, type, filename):
+    issue_dict = {}
+    issue_dict['project'] = {'key': project}
+    issue_dict['issuetype'] = {'name': type}
+    if epic:
+        issue_dict['customfield_10001'] = epic
+
+    with open(filename) as f:
+        content = f.readlines()
+
+    for line in content:
+        issue_dict['summary'] = line
+        new_issue = jira.create_issue(fields=issue_dict)
+
+cli = click.CommandCollection(sources=[cli1])
+
+if __name__ == '__main__':
+    cli()
--- jira.py
+++
@@ -1,5 +0,0 @@
-from jira.client import JIRA
-import click
-from secret import *
-
-jira = JIRA(options={'server': JIRA_SERVER}, basic_auth=(JIRA_USERNAME, JIRA_PASSWORD))
